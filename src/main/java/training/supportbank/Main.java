4bf3f7e9f004355e776abf39de46df5dafd3743e
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-02-27 16:42:35 +0000

    Added loop for invalid input

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
+
         Path path = FileSystems.getDefault().getPath("Transactions2014.csv");
 
         String line = "";
@@ -42,7 +43,6 @@ public class Main {
                                 transaction[3],
                                 Double.parseDouble(transaction[4]));
 
-                        System.out.println(accountList.get(from).getTransactionList().get(0).from);
                     } else {
                         Account account = new Account(transaction[1]);
                         accountList.add(account);
@@ -53,7 +53,6 @@ public class Main {
                                 transaction[3],
                                 Double.parseDouble(transaction[4]));
 
-                        System.out.println(account.getTransactionList().get(0).from);
                     }
 
                     if (to != -1) {
@@ -64,7 +63,6 @@ public class Main {
                                 transaction[3],
                                 Double.parseDouble(transaction[4]));
 
-                        System.out.println(accountList.get(to).getTransactionList().get(0).to);
                     } else {
                         Account account = new Account(transaction[2]);
                         accountList.add(account);
@@ -74,7 +72,6 @@ public class Main {
                                 transaction[2],
                                 transaction[3],
                                 Double.parseDouble(transaction[4]));
-                        System.out.println(account.getTransactionList().get(0).to);
                     }
                 }
             }
@@ -89,31 +86,59 @@ public class Main {
         System.out.println("Select an Option:");
         System.out.println("1. List All");
         System.out.println("2. List Specific User");
-        int i = scanner.nextInt();
+        int i = Integer.parseInt(scanner.nextLine());
+
+        double amountTheyOwe = 0;
+        double amountOwed = 0;
+
         if (i == 1) {
-//            for (Account account: accountList) {
-//                System.out.println();
-//                account.getTransactionList().
-//            }
+            for (Account account: accountList) {
+                System.out.println("Account name: " + account.getName());
+
+                for (Transaction transaction: account.getTransactionList()) {
+                    if (transaction.from.equals(account.getName())) {
+                        amountTheyOwe += transaction.amount;
+                    } else if (transaction.to.equals(account.getName())) {
+                        amountOwed += transaction.amount;
+                    }
+
+                }
+
+                System.out.println("Amount Owed: £" + new DecimalFormat("#.00").format(amountOwed));
+                System.out.println("Amount They Owe: £" + new DecimalFormat("#.00").format(amountTheyOwe));
+                System.out.println("");
+
+            }
         } else if (i == 2) {
+            String name;
+            int index;
+
+            boolean valid = false;
+
+            while (!valid){
+                System.out.println("Enter User Name");
+                name = scanner.nextLine();
+                index = indexOfAccountWithName(accountList, name);
+                if (accountList.get(index).getName().equals(name)){
+                    valid = true;
+                }
+            }
 
             System.out.println("Enter User Name");
-            String name = scanner.next();
-            int index = indexOfAccountWithName(accountList, name);
-            if (accountList.get(index).getName().contains(name)) {
+            name = scanner.nextLine();
+            index = indexOfAccountWithName(accountList, name);
+            if (accountList.get(index).getName().equals(name)) {
                 Account person = accountList.get(index);
-                System.out.println("Found Account: " + name);
+                System.out.println("Account Name: " + name);
                 System.out.println("Total Transactions: " + person.getTransactionList().toArray().length);
                 System.out.println("Transaction List");
-                double amountTheyOwe = 0;
-                double amountOwed = 0;
                 for (Transaction transaction : person.getTransactionList()) {
                     System.out.println(
                             "Date: " + transaction.date + ", " +
                                     "From: " + transaction.from + ", " +
                                     "To: " + transaction.to + ", " +
                                     "Amount Owed: " + transaction.amount + ", " +
-                            "Narrative: " + transaction.narrative);
+                                    "Narrative: " + transaction.narrative);
                 }
                 for (Transaction transaction: person.getTransactionList()){
                     if (transaction.from.equals(person.getName())){
@@ -122,8 +147,7 @@ public class Main {
                         amountOwed += transaction.amount;
                     }
                 }
-                System.out.println("Amount Owed: " + new DecimalFormat("#.00").format(amountOwed));
-                System.out.println("Amount They Owe: " + new DecimalFormat("#.00").format(amountTheyOwe));
+
             }
         } else {
             System.out.println("Invalid Option");
@@ -132,14 +156,12 @@ public class Main {
 
     private static int indexOfAccountWithName(List<Account> accountList, String name) {
         for (Account account : accountList) {
-            if (account.getName().contains(name)) {
+            if (account.getName().equals(name)) {
                 return accountList.indexOf(account);
             }
         }
         return -1;
     }
 
-    private static void loadfile() {
 
-    }
 }
